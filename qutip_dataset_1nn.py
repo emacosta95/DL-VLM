fix a bug in the f_density computation

qutip_dataset_1nn.py
@@ -146,6 +146,13 @@ parser.add_argument(
     default=1.0,
 )
 
+parser.add_argument(
+    "--rate",
+    type=float,
+    help="rate of the evolution of the disorder annealing (default=1.0)",
+    default=1.0,
+)
+
 args = parser.parse_args()
 
 
@@ -220,6 +227,36 @@ class DrivingPeriodic:
         return self.h[int(t / self.dt) - 1]
 
 
+class DrivingDisorder:
+    def __init__(
+        self,
+        t_resolution: int,
+        dt: float,
+        h_init: np.ndarray,
+        h_final: np.ndarray,
+        rate: float,
+        i: int,
+    ) -> None:
+        self.t_resolution = t_resolution
+        self.dt = dt
+
+        time = np.linspace(
+            -self.dt * self.t_resolution / 2,
+            self.dt * self.t_resolution / 2,
+            self.t_resolution,
+        )
+
+        self.h = (
+            h_init[None, :] * (1 - np.tanh(time * rate)[:, None]) / 2
+            + h_final[None, :] * (1 + np.tanh(time * rate)[:, None]) / 2
+        )  # initial field
+
+        self.i = i
+
+    def field(self, t: float, args) -> Union[np.ndarray, float]:
+        return self.h[int(t / self.dt) - 1, self.i]
+
+
 # size of the system
 size: int = args.size
 
@@ -247,54 +284,80 @@ else:
 n_dataset = args.n_dataset
 
 
-file_name = (
-    args.file_name
-    + f"_size_{size}_tf_{args.tf}_dt_{args.dt}_sigma_1_{args.sigma}_c_0_{args.c}_initial_field_{args.n_initial_field}_n_dataset_{n_dataset}"
-)
+if args.noise_type == "periodic":
+    file_name = (
+        args.file_name
+        + f"_size_{size}_tf_{args.tf}_dt_{args.dt}_a_{args.a}_omega_{args.omega}_initial_field_{args.n_initial_field}_n_dataset_{n_dataset}"
+    )
+elif args.noise_type == "disorder":
+    file_name = (
+        args.file_name
+        + f"_size_{size}_tf_{args.tf}_dt_{args.dt}_rate_{args.rate}_h_{np.e:.1f}_n_dataset_{n_dataset}"
+    )
 
 # initialization of the return value
 z: np.ndarray = np.zeros((n_dataset, t_resolution, size))
 hs: np.ndarray = np.zeros((n_dataset, t_resolution, size))
-x: np.ndarray =np.zeros((n_dataset, t_resolution, size))
-
+x: np.ndarray = np.zeros((n_dataset, t_resolution, size))
 
 
 # define the initial exp value
 obs: List[qutip.Qobj] = []
-obs_x: List[qutip.Qobj]=[]
+obs_x: List[qutip.Qobj] = []
 for i in range(size):
-    z_op= SpinOperator(index=[("z", i)], coupling=[1.0], size=size, verbose=1)
+    z_op = SpinOperator(index=[("z", i)], coupling=[1.0], size=size, verbose=1)
     # print(f"x[{i}]=", x.qutip_op, "\n")
-    x_op=SpinOperator(index=[("x", i)], coupling=[1.0], size=size, verbose=1)
+    x_op = SpinOperator(index=[("x", i)], coupling=[1.0], size=size, verbose=1)
     obs.append(z_op.qutip_op)
     obs_x.append(x_op.qutip_op)
 
-# initial constant field
-h0=np.random.uniform(-5,5,size=(args.n_initial_field))
-
 
 for sample in trange(n_dataset):
-    
+    # initial constant field
+    h0 = np.random.uniform(0, np.e, size=size)
+    hf = np.random.uniform(0, np.e, size=size)
     # define the initial time independent Hamiltonian
-    if sample % args.n_initial_field ==0:
+
+    if args.noise_type == "gaussian" and sample % args.n_initial_field == 0:
         ham0 = SpinHamiltonian(
-        direction_couplings=[("z", "z")],field_directions=[('x'),('z')], pbc=True, coupling_values=[1.],field_values=[1.,h0[sample % args.n_initial_field]] size=size
+            direction_couplings=[("z", "z")],
+            field_directions=[("x")],
+            pbc=True,
+            coupling_values=[1.0],
+            field_values=[1.0],
+            size=size,
         )
-        
+        # define the term \sum_i h_i z_i
+        ham_ext = SpinOperator(
+            index=[("z", i) for i in range(size)], coupling=h0, size=size
+        )
+
+    elif args.noise_type == "disorder":
+        ham0 = SpinHamiltonian(
+            direction_couplings=[("z", "z")],
+            field_directions=[("x")],
+            pbc=True,
+            coupling_values=[1.0],
+            field_values=[1.0],
+            size=size,
+        )
+        # define the term \sum_i h_i z_i
+        ham_ext = SpinOperator(
+            index=[("z", i) for i in range(size)], coupling=h0, size=size
+        )
+
     # define the instance
     ham = [ham0.qutip_op]
 
     # define the time dependent part
     # initialize the driving
-    driving.get_instance()
-    
-    
+
     if args.noise_type == "uniform":
         # initialize c0 and sigma
         if sample % args.different_gaussians == 0:
             c0 = np.random.uniform(0, args.c)
             sigma = np.random.uniform(1, args.sigma)
-            corr = c0 * np.exp(-1 * ((t[None, :] - t[:, None]) ** 2) / (2 * sigma ** 2))
+            corr = c0 * np.exp(-1 * ((t[None, :] - t[:, None]) ** 2) / (2 * sigma**2))
             e, l = np.linalg.eigh(corr)
         driving = DrivingUniformGaussian(
             t_resolution=t_resolution, dt=args.dt, e=e, l=l
@@ -311,37 +374,56 @@ for sample in trange(n_dataset):
                 sigma=sigma[sample % args.different_gaussians],
                 c=c[sample % args.different_gaussians],
             )
+
+            hs[sample, :, i] = driving.h + h0[sample % args.n_initial_field]
+            # we add the time dependent term in the Hamiltonian
+            # following the Qutip solver requests
+            ham.append([obs[i], driving.field])
+
         # periodic type
         elif args.noise_type == "periodic":
             driving = DrivingPeriodic(
                 t_resolution=t_resolution, dt=args.dt, a=args.a, omega=args.omega
             )
 
-        hs[sample, :, i] = driving.h
-        # we add the time dependent term in the Hamiltonian
-        # following the Qutip solver requests
-        ham.append([obs[i], driving.field])
+            hs[sample, :, i] = driving.h + h0[sample % args.n_initial_field]
+            # we add the time dependent term in the Hamiltonian
+            # following the Qutip solver requests
+            ham.append([obs[i], driving.field])
 
-    
+        elif args.noise_type == "disorder":
+            driving = DrivingDisorder(
+                t_resolution=t_resolution,
+                dt=args.dt,
+                h_init=h0,
+                h_final=hf,
+                rate=args.rate,
+                i=i,
+            )
+
+            hs[sample, :, :] = driving.h
+            # we add the time dependent term in the Hamiltonian
+            # following the Qutip solver requests
+            ham.append([obs[i], driving.field])
+
     # compute the initial state as groundstate of H0
-    eng, psi = np.linalg.eigh(ham0.qutip_op)
+    eng, psi = np.linalg.eigh(ham0.qutip_op + ham_ext.qutip_op)
     psi0 = counting_multiplicity(psi, eng)
-    # we take into account symmetries even if there shouldn't be 
+    # we take into account symmetries even if there shouldn't be
     # degeneracies
     psi0 = qutip.Qobj(psi0, shape=psi0.shape, dims=([[2 for i in range(size)], [1]]))
 
     # solver
-    output = qutip.sesolve(ham, psi0, t, e_ops=obs+obs_x)
+    output = qutip.sesolve(ham, psi0, t, e_ops=obs + obs_x)
     # upload the outcomes in z (density) and x
     for i in range(size):
         z[sample, :, i] = output.expect[i]
     for i in range(size):
-        x[sample,:,i]=output.expect[size+i]
+        x[sample, :, i] = output.expect[size + i]
 
     # save it every args.checkpoint times
     if sample % args.checkpoint == 0:
-        np.savez(file_name, density=z, potential=hs, time=t,transverse_magnetization=x)
+        np.savez(file_name, density=z, potential=hs, time=t, transverse_magnetization=x)
 
 # global save at the end
-np.savez(file_name, density=z, potential=hs, time=t,transverse_magnetization=x)
-
+np.savez(file_name, density=z, potential=hs, time=t, transverse_magnetization=x)

src/tddft_methods/adiabatic_tddft.py
@@ -4,21 +4,115 @@ import torch.nn as nn
 from src.training.conv_block_model import ConvBlock1D
 import numpy as np
 from typing import Tuple
+from tqdm import tqdm
+
+
+def pca_gradient(samples: torch.Tensor, gradient: torch.Tensor):
+    # define X^T (Meyer et al. 2020)
+    x = samples - gradient[None, :]
+    # implement C
+    c = torch.einsum("al,am->lm", x, x) / x.shape[0]
+    # diagonalize C
+    _, w = torch.linalg.eigh(c)
+    projection = torch.einsum("kl,km->lm", w[:2], w[:2])
+    return projection
+
+
+class QuantumAnnealing:
+    def __init__(
+        self,
+        z_init: torch.Tensor,
+        h: torch.Tensor,
+        e_target: torch.Tensor,
+        energy: nn.Module,
+        lr: float,
+        annealing_steps: int,
+    ) -> None:
+        pass
+
+        self.z_t: torch.Tensor = z_init  # batch x 0 x size
+        self.h: torch.Tensor = h  # batch x t_steps x size
+        self.e_target: torch.Tensor = e_target  # batch x t_steps x 0
+
+        self.energy: nn.Module = energy
+
+        self.lr: float = lr
+        self.annealing_steps: int = annealing_steps
+
+        self.z: torch.Tensor = torch.zeros_like(h)
+        self.eng: torch.Tensor = torch.zeros(size=(h.shape[0], h.shape[1]))
+
+    def annealing_step(self, z: torch.Tensor, h: torch.Tensor):
+        # -1,1 constrain
+        psi = torch.acos(z)
+        psi.requires_grad_(True)
+        for t in range(self.annealing_steps):
+            z = torch.cos(psi)
+            # energy functional
+            eng = self.energy(z, h)
+            eng.backward(torch.ones_like(eng))
+
+            with torch.no_grad():
+                grad = psi.grad  # batch x 0 x size
+                psi -= self.lr * grad
+                psi.grad.zero_()
+
+        return torch.cos(psi).detach(), eng
+
+    def run(
+        self,
+    ):
+        # initial configuration
+        self.z[:, 0, :] = self.z_t
+        eng0 = self.energy(self.z_t, self.h[:, 0])
+        self.eng[:, 0, :] = eng0
+
+        t_bar = tqdm(range(self.h[:, 1:, :].shape[1]))
+
+        # evolution
+        for t in t_bar:
+            self.z_t, eng_t = self.annealing_step(z=self.z_t, h=self.h[:, t + 1, :])
+            self.z[:, t + 1] = self.z_t
+            self.eng[:, t + 1] = eng_t.item()
+
+            t_bar.set_description(
+                f"de={torch.abs(self.eng[:,t+1]-self.e_target[:,t+1]).mean(0).item():.6f}"
+            )
+            t_bar.refresh()
 
 
 class AdiabaticTDDFT:
     def __init__(
-        self, model: torch.nn.Module, h: torch.tensor, psi0: torch.tensor
-    ) -> None:
-
-        self.psi = psi0
-        self.h = h
-        self.functional = model.double()
-        self.grad = 0.0
-
-    def gradient_descent_step(
         self,
-    ) -> tuple:
+        model: torch.nn.Module,
+        h: torch.tensor,
+        omega: float,
+        device: str,
+        samples_for_projection: torch.Tensor,
+    ) -> None:
+        # self.psi = psi0.to(device=device, dtype=torch.complex128)
+        self.h = h.to(device=device)
+        self.functional = model.double().to(device=device)
+        self.grad = 0.0
+        self.omega = omega
+        self.x_operator = torch.tensor(
+            [[0, 1], [1, 0]], dtype=torch.complex128, device=device
+        )
+        self.z_operator = torch.tensor(
+            [[-1, 0], [0, 1]], dtype=torch.complex128, device=device
+        )
+        self.identity = torch.tensor(
+            [[1, 0], [0, 1]], dtype=torch.complex128, device=device
+        )
+
+        # restore the gradient
+        self.sample_for_projection = samples_for_projection
+        self.gradient_old = torch.ones(h.shape[-1])
+
+        # save the f values
+        self.f_values = 0.0
+
+    def gradient_descent_step(self, psi: torch.Tensor) -> tuple:
         """This routine computes the step of the gradient using both the positivity and the nomralization constrain
 
         Arguments:
@@ -32,201 +126,199 @@ class AdiabaticTDDFT:
 
         # print(torch.typename(self.psi), torch.typename(self.sigmaz))
         # self.psi = self.psi.to(torch.double
-        phi = self.psi.clone()
+        phi = psi.clone()
 
-        w = 1 - 2 * torch.conj(phi) * phi
-        print(w)
-        w = w.to(torch.double)
+        w = torch.real(
+            torch.conj(phi[:, 0]) * phi[:, 0] - torch.conj(phi[:, 1]) * phi[:, 1]
+        )
+        w = w.to(dtype=torch.double)
         w.requires_grad_(True)
-        f = self.functional(w)
-        print("func=", f)
+        f = self.functional(w.view(1, -1))  # batch size form 1 x l
+        f = f[0].sum(-1)
+        self.f_values = f.clone()
         f.backward(torch.ones_like(f))
         with torch.no_grad():
-
             grad = w.grad
             self.grad = grad.clone()
-            # print("psi=", self.psi)
-            # print("grad=", grad)
-
+            # projection = pca_gradient(
+            #    samples=self.sample_for_projection, gradient=self.grad
+            # )
+            # self.grad = torch.einsum("lm,m->l", projection, self.grad)
             w.grad.zero_()
 
-    def kutta_step(self, vector: torch.Tensor, t: float, dt: float):
-
-        vector.to(torch.complex128)
-        field = self.h[:, int(t / dt) + 1] + self.grad
-        output = torch.einsum("al,al->al", field, vector)
-        # print(self.grad)
-        return -1j * (output)
-
-    def time_step(self, dt: float, t: float):
-
+    def time_step(self, dt: float, t: float, psi: torch.Tensor):
         # non linear term
-        self.gradient_descent_step()
+        self.gradient_descent_step(psi=psi)
 
-        # runge kutta
-        k1 = self.kutta_step(self.psi.to(torch.complex128), t=t, dt=dt)
-        # print("k1=", k1)
-        k2 = self.kutta_step(self.psi + dt * k1 / 2, t=t + dt / 2, dt=dt)
-        # print("k2=", k2)
-        k3 = self.kutta_step(self.psi + dt * k2 / 2, t=t + dt / 2, dt=dt)
-        # print("k2=", k3)
-        k4 = self.kutta_step(self.psi + dt * k3, t + dt, dt)
-        # print("k2=", k4)
-        self.psi = self.psi + (1 / 6) * (k1 + 2 * k2 + 2 * k3 + k4) * dt
+        # Crank-Nicholson algorithm
+        field = self.h[int(t / dt)] + self.grad
+        field = field.to(dtype=torch.complex128)
+        ext_field = field[:, None, None] * self.z_operator[None, :, :]
+        unitary_op = self.identity[None, :, :] + 0.5j * dt * (
+            self.omega * self.x_operator[None, :, :] + ext_field
+        )
+        unitary_op_star = self.identity[None, :, :] - 0.5j * dt * (
+            self.omega * self.x_operator[None, :, :] + ext_field
+        )
+        unitary = torch.einsum(
+            "iab,ibc->iac", torch.linalg.inv(unitary_op), unitary_op_star
+        )
+        psi = torch.einsum("iab,ib->ia", unitary, psi)
 
-    def compute_magnetization(self):
-        z = 1 - 2 * torch.conj(self.psi) * self.psi
+        # impose the norm
+        psi = psi / torch.linalg.norm(psi, dim=-1)[:, None]
+
+        # self.psi = self.psi / torch.norm(self.psi, dim=-1)[:, None]
+
+        return psi
+
+    def compute_magnetization(self, psi: torch.Tensor):
+        z = torch.conj(psi[:, 0]) * psi[:, 0] - torch.conj(psi[:, 1]) * psi[:, 1]
         return z.view(-1)
 
 
-class AdiabaticTDDFTNN(nn.Module):
-    def __init__(
-        self,
-        n_conv: int,
-        activation: nn.Module,
-        hc: int,
-        in_channels: int,
-        kernel_size: int,
-        padding_mode: str,
-        out_channels: int,
-        j: float,
-        tf: float,
-        dt: float,
-        time_interval: int,
-        device: str,
-    ) -> None:
-        super().__init__()
+# class AdiabaticTDDFTNN(nn.Module):
+#     def __init__(
+#         self,
+#         n_conv: int,
+#         activation: nn.Module,
+#         hc: int,
+#         in_channels: int,
+#         kernel_size: int,
+#         padding_mode: str,
+#         out_channels: int,
+#         j: float,
+#         tf: float,
+#         dt: float,
+#         time_interval: int,
+#         device: str,
+#     ) -> None:
+#         super().__init__()
 
-        self.v_adiabatic = ConvBlock1D(
-            n_conv=n_conv,
-            hc=hc,
-            in_channels=in_channels,
-            kernel_size=kernel_size,
-            padding_mode=padding_mode,
-            out_channels=out_channels,
-            activation=activation,
-        )
+#         self.v_adiabatic = ConvBlock1D(
+#             n_conv=n_conv,
+#             hc=hc,
+#             in_channels=in_channels,
+#             kernel_size=kernel_size,
+#             padding_mode=padding_mode,
+#             out_channels=out_channels,
+#             activation=activation,
+#         )
 
-        self.device = device
+#         self.device = device
 
-        self.j = j
+#         self.j = j
 
-        self.loss = nn.MSELoss()
+#         self.loss = nn.MSELoss()
 
-        self.time = torch.linspace(0, tf, int(tf / dt))[:time_interval]
+#         self.time = torch.linspace(0, tf, int(tf / dt))[:time_interval]
 
-    def time_step(self, dt: float, t: float, psi: torch.Tensor, h: torch.Tensor):
+#     def time_step(self, dt: float, t: float, psi: torch.Tensor, h: torch.Tensor):
+#         # non linear term
+#         z = 1 - 2 * torch.einsum("aml,alm->al", torch.conj(psi), psi)
+#         z = z.to(torch.double)
+#         v = self.forward(z)
 
-        # non linear term
-        z = 1 - 2 * torch.einsum("aml,alm->al", torch.conj(psi), psi)
-        z = z.to(torch.double)
-        v = self.forward(z)
+#         # runge kutta
+#         field = v + h[:, int(t / dt)]
+#         k1 = self.kutta_step(vector=psi.to(torch.complex128), field=field)
+#         field = v + h[:, int(t / dt + 1 / 2)]
+#         k2 = self.kutta_step(vector=psi + dt * k1 / 2, field=field)
+#         k3 = self.kutta_step(vector=psi + dt * k2 / 2, field=field)
+#         field = v + h[:, int(t / dt + 1)]
+#         k4 = self.kutta_step(vector=psi + dt * k3, field=field)
+#         psi = psi + (1 / 6) * (k1 * dt + 2 * k2 + 2 * k3 + k4) * dt
+#         return psi
 
-        # runge kutta
-        field = v + h[:, int(t / dt)]
-        k1 = self.kutta_step(vector=psi.to(torch.complex128), field=field)
-        field = v + h[:, int(t / dt + 1 / 2)]
-        k2 = self.kutta_step(vector=psi + dt * k1 / 2, field=field)
-        k3 = self.kutta_step(vector=psi + dt * k2 / 2, field=field)
-        field = v + h[:, int(t / dt + 1)]
-        k4 = self.kutta_step(vector=psi + dt * k3, field=field)
-        psi = psi + (1 / 6) * (k1 * dt + 2 * k2 + 2 * k3 + k4) * dt
-        return psi
+#     def kutta_step(self, field: torch.Tensor, vector: torch.Tensor):
+#         vector.to(torch.complex128)
+#         matrix = -1j * (
+#             self.laplacian[None, :, :]
+#             + torch.einsum(
+#                 "lm,am->alm",
+#                 torch.eye(field.shape[-1]).to(torch.double).to(self.device),
+#                 field,
+#             )
+#         ).to(torch.complex128)
+#         output = torch.einsum("aij,amj->ami", matrix, vector)
 
-    def kutta_step(self, field: torch.Tensor, vector: torch.Tensor):
+#         return output
 
-        vector.to(torch.complex128)
-        matrix = (
-            -1j
-            * (
-                self.laplacian[None, :, :]
-                + torch.einsum(
-                    "lm,am->alm",
-                    torch.eye(field.shape[-1]).to(torch.double).to(self.device),
-                    field,
-                )
-            ).to(torch.complex128)
-        )
-        output = torch.einsum("aij,amj->ami", matrix, vector)
+#     def forward(self, x: torch.tensor):
+#         x = x.unsqueeze(1)
+#         x = self.v_adiabatic(x)
+#         x = x.squeeze(1)
+#         return x
 
-        return output
+#     def get_magnetization(self, psi: torch.tensor):
+#         z = 1 - 2 * torch.einsum("ami,ami->ai", torch.conj(psi), psi).to(torch.double)
 
-    def forward(self, x: torch.tensor):
-        x = x.unsqueeze(1)
-        x = self.v_adiabatic(x)
-        x = x.squeeze(1)
-        return x
+#         return z
 
-    def get_magnetization(self, psi: torch.tensor):
-        z = 1 - 2 * torch.einsum("ami,ami->ai", torch.conj(psi), psi).to(torch.double)
+#     def time_evolution(self, h: torch.Tensor):
+#         size = h.shape[-1]
+#         time = self.time
+#         # laplacian
+#         self.laplacian = torch.zeros(size=(size, size)).to(
+#             dtype=torch.double, device=self.device
+#         )
+#         self.laplacian[torch.arange(size), torch.arange(size)] = 2.0
+#         self.laplacian[
+#             (torch.arange(size) + 1) % size, (torch.arange(size)) % size
+#         ] = -1
+#         self.laplacian[
+#             (torch.arange(size) - 1) % size, (torch.arange(size)) % size
+#         ] = -1
+#         self.laplacian = self.j * self.laplacian
 
-        return z
+#         z_ml = torch.zeros_like(h).to(torch.double).to(self.device)
+#         dt = torch.abs(time[1] - time[0])
+#         psi = (
+#             torch.zeros((h.shape[0], h.shape[-1], h.shape[-1]))
+#             .to(torch.double)
+#             .to(self.device)
+#         )
+#         psi[:, 0, :] = np.sqrt(0.5)
 
-    def time_evolution(self, h: torch.Tensor):
+#         z_ml = self.get_magnetization(psi).view(-1, 1, size)
+#         for i, t in enumerate(time[:-1]):
+#             psi = self.time_step(dt=dt, t=t, psi=psi, h=h)
+#             z_ml = torch.cat(
+#                 (self.get_magnetization(psi).view(-1, 1, size), z_ml), axis=1
+#             )
+#         return z_ml, psi
 
-        size = h.shape[-1]
-        time = self.time
-        # laplacian
-        self.laplacian = torch.zeros(size=(size, size)).to(
-            dtype=torch.double, device=self.device
-        )
-        self.laplacian[torch.arange(size), torch.arange(size)] = 2.0
-        self.laplacian[
-            (torch.arange(size) + 1) % size, (torch.arange(size)) % size
-        ] = -1
-        self.laplacian[
-            (torch.arange(size) - 1) % size, (torch.arange(size)) % size
-        ] = -1
-        self.laplacian = self.j * self.laplacian
+#     def initialization(self, h):
+#         size = h.shape[-1]
+#         time = self.time
+#         # laplacian
+#         self.laplacian = torch.zeros(size=(size, size)).to(
+#             dtype=torch.double, device=self.device
+#         )
+#         self.laplacian[torch.arange(size), torch.arange(size)] = 2.0
+#         self.laplacian[
+#             (torch.arange(size) + 1) % size, (torch.arange(size)) % size
+#         ] = -1
+#         self.laplacian[
+#             (torch.arange(size) - 1) % size, (torch.arange(size)) % size
+#         ] = -1
+#         self.laplacian = self.j * self.laplacian
 
-        z_ml = torch.zeros_like(h).to(torch.double).to(self.device)
-        dt = torch.abs(time[1] - time[0])
-        psi = (
-            torch.zeros((h.shape[0], h.shape[-1], h.shape[-1]))
-            .to(torch.double)
-            .to(self.device)
-        )
-        psi[:, 0, :] = np.sqrt(0.5)
+#         z_ml = torch.zeros_like(h).to(torch.double).to(self.device)
+#         dt = torch.abs(time[1] - time[0])
+#         psi = (
+#             torch.zeros((h.shape[0], h.shape[-1], h.shape[-1]))
+#             .to(torch.double)
+#             .to(self.device)
+#         )
+#         psi[:, 0, :] = np.sqrt(0.5)
 
-        z_ml = self.get_magnetization(psi).view(-1, 1, size)
-        for i, t in enumerate(time[:-1]):
-            psi = self.time_step(dt=dt, t=t, psi=psi, h=h)
-            z_ml = torch.cat(
-                (self.get_magnetization(psi).view(-1, 1, size), z_ml), axis=1
-            )
-        return z_ml, psi
+#         return z_ml, psi
 
-    def initialization(self, h):
-        size = h.shape[-1]
-        time = self.time
-        # laplacian
-        self.laplacian = torch.zeros(size=(size, size)).to(
-            dtype=torch.double, device=self.device
-        )
-        self.laplacian[torch.arange(size), torch.arange(size)] = 2.0
-        self.laplacian[
-            (torch.arange(size) + 1) % size, (torch.arange(size)) % size
-        ] = -1
-        self.laplacian[
-            (torch.arange(size) - 1) % size, (torch.arange(size)) % size
-        ] = -1
-        self.laplacian = self.j * self.laplacian
-
-        z_ml = torch.zeros_like(h).to(torch.double).to(self.device)
-        dt = torch.abs(time[1] - time[0])
-        psi = (
-            torch.zeros((h.shape[0], h.shape[-1], h.shape[-1]))
-            .to(torch.double)
-            .to(self.device)
-        )
-        psi[:, 0, :] = np.sqrt(0.5)
-
-        return z_ml, psi
-
-    def train_step(self, batch: Tuple, device: str):
-        h, z = batch
-        h = h.to(device=device, dtype=torch.double)
-        z = z.to(device=device, dtype=torch.double)
-        z_ml = self.time_evolution(h=h, time=self.time)
-        loss = self.loss(z, z_ml)
-        return loss, z_ml
+#     def train_step(self, batch: Tuple, device: str):
+#         h, z = batch
+#         h = h.to(device=device, dtype=torch.double)
+#         z = z.to(device=device, dtype=torch.double)
+#         z_ml = self.time_evolution(h=h, time=self.time)
+#         loss = self.loss(z, z_ml)
+#         return loss, z_ml