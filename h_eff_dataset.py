2f05535c72856f416f985629e489ac68da9cd02b
Emanuele Costa <emanuele.costa@unicam.it>
2024-06-21 09:53:59 +0000

    make the training dataset smart

--- h_eff_dataset.py
+++ h_eff_dataset.py
@@ -70,30 +70,23 @@ l = 8
 
 rate = 1.0
 # j coupling
-j = -1
+j = 1
 # omega auxiliary field
 omega = 1
-range_h = np.linspace(0.6, 1.0, nbatch)
 
-# max_h = 1.0 + np.linspace(0.1, 1., nbatch)
-# min_h = np.linspace(0.1, 1., nbatch)
 
-steps = 400
-tf = 40.0
+steps = 200
+tf = 20.0
 time = np.linspace(0.0, tf, steps)
 
 # z_qutip_tot = np.zeros((nbatch * nbatch * batch_size, steps, l))
 z_qutip_tot = np.zeros((nbatch * batch_size, steps, l))
 z_big_tot = np.zeros_like(z_qutip_tot)
 h_eff_tot = np.zeros_like(z_qutip_tot)
-h_eff_observables_tot = np.zeros_like(z_qutip_tot)
 h_tot = np.zeros_like(z_qutip_tot)
 current_qutip_tot = np.zeros_like(z_qutip_tot)
 current_derivative_tot = np.zeros_like(z_qutip_tot)
-q_tot = np.zeros_like(z_qutip_tot)
-r_diag_tot = np.zeros_like(z_qutip_tot)
-r_diag_minus_tot = np.zeros_like(z_qutip_tot)
-r_diag_plus_tot = np.zeros_like(z_qutip_tot)
+
 x_sp_tot = np.zeros_like(z_qutip_tot)
 
 ham0 = SpinHamiltonian(
@@ -107,10 +100,7 @@ hamExtX = SpinOperator(index=[("x", i) for i in range(l)], coupling=[omega] * l,
 
 obs: List[qutip.Qobj] = []
 current_obs: List[qutip.Qobj] = []
-q_obs: List[qutip.Qobj] = []
-r_diag_obs: List[qutip.Qobj] = []
-r_diag_plus_obs: List[qutip.Qobj] = []
-r_diag_minus_obs: List[qutip.Qobj] = []
+
 for i in range(l):
     z_op = SpinOperator(index=[("z", i)], coupling=[1.0], size=l, verbose=1)
     # print(f"x[{i}]=", x.qutip_op, "\n")
@@ -119,55 +109,34 @@ for i in range(l):
         coupling=[2 * j, 2 * j],
         size=l,
     )
-    q = SpinOperator(
-        index=[("x", (i - 1) % l, "z", i, "x", (i + 1) % l)],
-        coupling=[2 * j**2],
-        size=l,
-    )
-    r_diag = SpinOperator(
-        index=[("x", (i - 1) % l, "x", i), ("x", i, "x", (i + 1) % l)],
-        coupling=[j, j],
-        size=l,
-    )
-    r_diag_plus = SpinOperator(
-        index=[("y", i, "y", (i + 1) % l)],
-        coupling=[-j, -j],
-        size=l,
-    )
-    r_diag_minus = SpinOperator(
-        index=[("y", (i - 1) % l, "y", i)],
-        coupling=[-j, -j],
-        size=l,
-    )
 
     obs.append(z_op.qutip_op)
     current_obs.append(current.qutip_op)
-    q_obs.append(q.qutip_op)
-    r_diag_obs.append(r_diag.qutip_op)
-    r_diag_minus_obs.append(r_diag_minus.qutip_op)
-    r_diag_plus_obs.append(r_diag_plus.qutip_op)
+
 
 for idx_batch in trange(0, nbatch):
-    # for jdx_batch in trange(0, nbatch):
-    # hi = np.random.uniform(min_h[idx_batch], max_h[idx_batch], size=(batch_size, l))
-    # hf = np.random.uniform(min_h[jdx_batch], max_h[jdx_batch], size=(batch_size, l))
 
-    # random configurations
-    # hi = np.random.uniform(
-    #    range_h[idx_batch], range_h[idx_batch] + 1.0, size=(batch_size, l)
-    # )
-    hi = np.ones(l)  # we fix the initial field to be 1J
-    delta = np.random.uniform(0.1, 0.8, size=(batch_size, l))
-    # delta_uniform = np.random.uniform(0.1, 0.8, size=(batch_size))
+    hi = np.ones((time.shape[0], l))  # we fix the initial field to be 1J
 
     for idx in trange(0, batch_size):
 
-        rate = np.random.uniform(0.3, 1.0)
+        # rate = np.random.uniform(0.3, 1.0)
 
+        omega = np.linspace(0, np.pi, time.shape[0] // 4)
+        omega_cutoff = 16
+        delta = np.random.uniform(size=(omega_cutoff, l))
         # random_int = np.random.randint(len(rates))
         # rate = rates[random_int]
         # h = delta_uniform[idx, None, None] * np.sin(time * rate)[:, None] + hi[None, :]
-        h = delta[idx, None, :] * np.sin(time * rate)[:, None] + hi[None, :]
+        h = (
+            delta[:, None, :]
+            * np.sin(time[None, :, None] * omega[:omega_cutoff, None, None])
+            + hi
+        )
+
+        h = np.average(h, axis=0)
+        # we define a random h
+
         # h = (
         #    np.exp(-1 * rate * time)[:, None] * hi[idx, None, :]
         #    + (1 - np.exp(-1 * rate * time)[:, None]) * hf[idx, None, :]
@@ -183,18 +152,7 @@ for idx_batch in trange(0, nbatch):
         )
 
         print("real ground state energy=", eng[0])
-        # to check if we have the same outcome with the Crank-Nicholson algorithm
-        # psi = initialize_psi_from_z_and_x(z=-1 * zi[0], x=zi[1])
-        # psi = psi.detach().numpy()
-        # for i in range(l):
-        #     psi_l = qutip.Qobj(psi[i], shape=psi[i].shape, dims=([[2], [1]]))
-        #     if i == 0:
-        #         psi0 = psi_l
-        #     else:
-        #         psi0 = qutip.tensor(psi0, psi_l)
-        # compute and check the magnetizations
 
-        # build up the time dependent object for the qutip evolution
         hamiltonian = [ham0.qutip_op + hamExtX.qutip_op]
 
         for i in range(l):
@@ -208,48 +166,17 @@ for idx_batch in trange(0, nbatch):
 
         # evolution and
 
-        output = qutip.sesolve(
-            hamiltonian,
-            psi0,
-            time,
-            e_ops=obs
-            + current_obs
-            + q_obs
-            + r_diag_obs
-            + r_diag_minus_obs
-            + r_diag_plus_obs,
-        )
+        output = qutip.sesolve(hamiltonian, psi0, time, e_ops=obs + current_obs)
 
         current_exp = np.zeros((steps, l))
         z_exp = np.zeros_like(current_exp)
-        r_diag_exp = np.zeros_like(current_exp)
-        r_diag_minus_exp = np.zeros_like(current_exp)
-        r_diag_plus_exp = np.zeros_like(current_exp)
+
         for r in range(l):
             z_exp[:, r] = output.expect[r]
             current_exp[:, r] = output.expect[l + r]
-            r_diag_exp[:, r] = output.expect[3 * l + r]
-            r_diag_minus_exp[:, r] = output.expect[4 * l + r]
-            r_diag_plus_exp[:, r] = output.expect[5 * l + r]
 
         # Current derivative
         current_derivative = np.gradient(current_exp, time, axis=0)
-
-        # R matrix
-        r_matrix_1 = np.einsum("ij,tj->tij", np.eye(r_diag_exp.shape[-1]), r_diag_exp)
-        # +1 off diagonal term
-        r_matrix_2 = np.einsum(
-            "ij,tj->tij", np.eye(r_diag_exp.shape[-1]), r_diag_plus_exp
-        )
-        r_matrix_2 = np.roll(r_matrix_2, axis=-1, shift=1)
-        # -1 off diagonal term
-        r_matrix_3 = np.einsum(
-            "ij,tj->tij", np.eye(r_diag_exp.shape[-1]), r_diag_minus_exp
-        )
-        r_matrix_3 = np.roll(r_matrix_3, axis=-1, shift=-1)
-
-        # sum of all this term
-        r_matrix = r_matrix_3 + r_matrix_2 + r_matrix_1
 
         # compute the effective field
         x_sp = np.sqrt(1 - z_exp**2) * np.cos(
@@ -259,59 +186,32 @@ for idx_batch in trange(0, nbatch):
         current_derivative = np.gradient(current_exp, time, axis=0)
         h_eff = (0.25 * current_derivative + z_exp) / (x_sp + 10**-4)
 
-        # build the operators that defines the derivative of the current
-        r_h = np.einsum(
-            "tji,ti->tj", r_matrix, h
-        )  # +np.einsum('tji,ti->ti',r_matrix,h))
-
-        q_effective = (
-            r_h - current_derivative / 4
-        )  # factor 4 due to the commutation relations
-
-        # definition of the heff from observables
-        h_eff_from_observables = (z_exp + (r_h - q_effective)) / (x_sp + 10**-4)
-
         # update the database
         h_eff_tot[(batch_size * (idx_batch) + idx)] = h_eff
-        h_eff_observables_tot[(batch_size * (idx_batch) + idx)] = h_eff_from_observables
         h_tot[(batch_size * (idx_batch) + idx)] = h
         z_qutip_tot[(batch_size * (idx_batch) + idx)] = z_exp
         current_qutip_tot[(batch_size * (idx_batch) + idx)] = current_exp
         x_sp_tot[(batch_size * (idx_batch) + idx)] = x_sp
-        q_tot[(batch_size * (idx_batch) + idx)] = q_effective
-        r_diag_tot[(batch_size * (idx_batch) + idx)] = r_diag_exp
-        r_diag_plus_tot[(batch_size * (idx_batch) + idx)] = r_diag_plus_exp
-        r_diag_minus_tot[(batch_size * (idx_batch) + idx)] = r_diag_minus_exp
         current_derivative_tot[(batch_size * (idx_batch) + idx)] = current_derivative
 
     np.savez(
-        f"data/dataset_h_eff/periodic/xxzx_model/dataset_periodic_random_rate_03-1_random_amplitude_01-08_fixed_initial_state_nbatch_{nbatch}_batchsize_{batch_size}_steps_{steps}_tf_{tf}_l_{l}_240601",
+        f"data/dataset_h_eff/periodic/xxzx_model/dataset_random_rate_random_amplitude_01-08_fixed_initial_state_nbatch_{nbatch}_batchsize_{batch_size}_steps_{steps}_tf_{tf}_l_{l}_240620",
         current=current_qutip_tot,
         z=z_qutip_tot,
         h_eff=h_eff_tot,
         current_derivative=current_derivative_tot,
         h=h_tot,
         x_sp=x_sp_tot,
-        q=q_tot,
-        r_diag=r_diag_tot,
-        r_diag_minus=r_diag_minus_tot,
-        r_diag_plus=r_diag_plus_tot,
-        h_eff_from_observables=h_eff_observables_tot,
         time=time,
     )
 
 np.savez(
-    f"data/dataset_h_eff/periodic/xxzx_model/dataset_periodic_random_rate_03-1_random_amplitude_01-08_fixed_initial_state_nbatch_{nbatch}_batchsize_{batch_size}_steps_{steps}_tf_{tf}_l_{l}_240601",
+    f"data/dataset_h_eff/periodic/xxzx_model/dataset_random_rate_random_amplitude_01-08_fixed_initial_state_nbatch_{nbatch}_batchsize_{batch_size}_steps_{steps}_tf_{tf}_l_{l}_240620",
     current=current_qutip_tot,
     z=z_qutip_tot,
     h_eff=h_eff_tot,
     current_derivative=current_derivative_tot,
     h=h_tot,
     x_sp=x_sp_tot,
-    q=q_tot,
-    r_diag=r_diag_tot,
-    r_diag_minus=r_diag_minus_tot,
-    r_diag_plus=r_diag_plus_tot,
-    h_eff_from_observables=h_eff_observables_tot,
     time=time,
 )
--- run_train.sh
+++ run_train.sh
@@ -1,11 +1,11 @@
-time_interval=200
+time_interval=100
 
 
 
 
 # nohup python train.py --time_interval=$time_interval  --model_type="PixelCNN" --input_channels=1 --output_channels=1 --input_size 8 200 --kernel_size 5 7 --padding 2 3 --pooling=0    --data_path 'data/dataset_h_eff/train_dataset_pixelcnn_ndata_500000_20240520.npz'  --model_name=kohm_sham/cnn_field2field/PixelCNN_field2field_time_steps_100_tf_20_240601_dataset_500k --hidden_channels 80 80 80 80 --keys input_data output_data    --epochs=2000      > output_model_field2field_pixelcnn_1.txt &
 
-nohup python train.py --time_interval=$time_interval  --model_type="LSTM" --input_channels=1 --output_channels=1 --input_size 8 --kernel_size 0 --padding 0 --pooling=0    --data_path 'data/dataset_h_eff/periodic/xxzx_model/dataset_periodic_random_rate_03-1_random_amplitude_01-08_fixed_initial_state_nbatch_1_batchsize_20000_steps_400_tf_40.0_l_8_240601.npz'  --model_name=kohm_sham/cnn_field2field/LSTM_field2field_xxzx_time_steps_200_tf_40_240603_dataset_20k --hidden_channels 200 200 200 200 200 200 200  --keys h h_eff    --epochs=2000      > output_model_field2field_lstm_1.txt &
+nohup python train.py --time_interval=$time_interval  --model_type="LSTM" --input_channels=1 --output_channels=1 --input_size 8 --kernel_size 0 --padding 0 --pooling=0    --data_path 'data/dataset_h_eff/periodic/xxzx_model/dataset_random_rate_random_amplitude_01-08_fixed_initial_state_nbatch_1_batchsize_20000_steps_400_tf_40.0_l_8_240607.npz'  --model_name=kohm_sham/cnn_field2field/LSTM_field2field_random_driving_xxzx_time_steps_100_tf_40_240620_dataset_20k --hidden_channels 200 200 200 200 200 200 200  --keys h h_eff    --epochs=2000      > output_model_field2field_lstm_1.txt &
 
 
 # nohup python train.py --time_interval=$time_interval  --model_type="REDENTnopooling" --input_channels=1 --output_channels=4 --input_size 8 --kernel_size 5 --padding 2 --pooling=0    --data_path 'data/dataset_h_eff/train_dataset_punctual_periodic_driving_200000.npz'  --model_name=kohm_sham/cnn_density2field/REDENTnopooling_density2field_time_steps_200_tf_20_240602_dataset_200k --hidden_channels 40 40 40 40  --keys density F_density    --epochs=2000      > output_model_density2field_redent_1.txt &
