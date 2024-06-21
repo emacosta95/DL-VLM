e92aad3594ba34218478ab5be4843bfbfd741d4e
Emanuele Costa <emanuele.costa@unicam.it>
2024-06-21 10:14:50 +0000

    restore visualization

--- h_eff_dataset.py
+++ h_eff_dataset.py
@@ -80,14 +80,12 @@ tf = 20.0
 time = np.linspace(0.0, tf, steps)
 
 # z_qutip_tot = np.zeros((nbatch * nbatch * batch_size, steps, l))
-z_qutip_tot = np.zeros((nbatch * batch_size, steps, l))
-z_big_tot = np.zeros_like(z_qutip_tot)
-h_eff_tot = np.zeros_like(z_qutip_tot)
-h_tot = np.zeros_like(z_qutip_tot)
-current_qutip_tot = np.zeros_like(z_qutip_tot)
-current_derivative_tot = np.zeros_like(z_qutip_tot)
-
-x_sp_tot = np.zeros_like(z_qutip_tot)
+z_qutip_tot = []
+h_eff_tot = []
+h_tot = []
+current_qutip_tot = []
+current_derivative_tot = []
+x_sp_tot = []
 
 ham0 = SpinHamiltonian(
     direction_couplings=[("x", "x")],
@@ -113,97 +111,83 @@ for i in range(l):
     obs.append(z_op.qutip_op)
     current_obs.append(current.qutip_op)
 
+hi = np.ones((time.shape[0], l))  # we fix the initial field to be 1J
 
-for idx_batch in trange(0, nbatch):
+for idx in trange(0, batch_size):
 
-    hi = np.ones((time.shape[0], l))  # we fix the initial field to be 1J
+    # rate = np.random.uniform(0.3, 1.0)
 
-    for idx in trange(0, batch_size):
+    omega = np.linspace(0, np.pi, time.shape[0] // 4)
+    omega_cutoff = 16
+    delta = np.random.uniform(size=(omega_cutoff, l))
 
-        # rate = np.random.uniform(0.3, 1.0)
-
-        omega = np.linspace(0, np.pi, time.shape[0] // 4)
-        omega_cutoff = 16
-        delta = np.random.uniform(size=(omega_cutoff, l))
-        # random_int = np.random.randint(len(rates))
-        # rate = rates[random_int]
-        # h = delta_uniform[idx, None, None] * np.sin(time * rate)[:, None] + hi[None, :]
-        h = (
-            delta[:, None, :]
-            * np.sin(time[None, :, None] * omega[:omega_cutoff, None, None])
-            + hi
-        )
-
-        h = np.average(h, axis=0)
-        # we define a random h
-
-        # h = (
-        #    np.exp(-1 * rate * time)[:, None] * hi[idx, None, :]
-        #    + (1 - np.exp(-1 * rate * time)[:, None]) * hf[idx, None, :]
-        # )
-
-        hamExtZ = SpinOperator(
-            index=[("z", i) for i in range(l)], coupling=h[0], size=l
-        )
-
-        eng, psi0 = np.linalg.eigh(ham0.qutip_op + hamExtZ.qutip_op + hamExtX.qutip_op)
-        psi0 = qutip.Qobj(
-            psi0[:, 0], shape=psi0.shape, dims=([[2 for i in range(l)], [1]])
-        )
-
-        print("real ground state energy=", eng[0])
-
-        hamiltonian = [ham0.qutip_op + hamExtX.qutip_op]
-
-        for i in range(l):
-            drive_z = Driving(
-                h=h,
-                dt=time[1] - time[0],
-                idx=i,
-            )
-
-            hamiltonian.append([obs[i], drive_z.field])
-
-        # evolution and
-
-        output = qutip.sesolve(hamiltonian, psi0, time, e_ops=obs + current_obs)
-
-        current_exp = np.zeros((steps, l))
-        z_exp = np.zeros_like(current_exp)
-
-        for r in range(l):
-            z_exp[:, r] = output.expect[r]
-            current_exp[:, r] = output.expect[l + r]
-
-        # Current derivative
-        current_derivative = np.gradient(current_exp, time, axis=0)
-
-        # compute the effective field
-        x_sp = np.sqrt(1 - z_exp**2) * np.cos(
-            np.arcsin(-1 * (current_exp) / (2 * np.sqrt(1 - z_exp**2)))
-        )
-
-        current_derivative = np.gradient(current_exp, time, axis=0)
-        h_eff = (0.25 * current_derivative + z_exp) / (x_sp + 10**-4)
-
-        # update the database
-        h_eff_tot[(batch_size * (idx_batch) + idx)] = h_eff
-        h_tot[(batch_size * (idx_batch) + idx)] = h
-        z_qutip_tot[(batch_size * (idx_batch) + idx)] = z_exp
-        current_qutip_tot[(batch_size * (idx_batch) + idx)] = current_exp
-        x_sp_tot[(batch_size * (idx_batch) + idx)] = x_sp
-        current_derivative_tot[(batch_size * (idx_batch) + idx)] = current_derivative
-
-    np.savez(
-        f"data/dataset_h_eff/periodic/xxzx_model/dataset_random_rate_random_amplitude_01-08_fixed_initial_state_nbatch_{nbatch}_batchsize_{batch_size}_steps_{steps}_tf_{tf}_l_{l}_240620",
-        current=current_qutip_tot,
-        z=z_qutip_tot,
-        h_eff=h_eff_tot,
-        current_derivative=current_derivative_tot,
-        h=h_tot,
-        x_sp=x_sp_tot,
-        time=time,
+    h = (
+        delta[:, None, :]
+        * np.sin(time[None, :, None] * omega[:omega_cutoff, None, None])
+        + hi
     )
+
+    h = np.average(h, axis=0)
+
+    hamExtZ = SpinOperator(index=[("z", i) for i in range(l)], coupling=h[0], size=l)
+
+    eng, psi0 = np.linalg.eigh(ham0.qutip_op + hamExtZ.qutip_op + hamExtX.qutip_op)
+    psi0 = qutip.Qobj(psi0[:, 0], shape=psi0.shape, dims=([[2 for i in range(l)], [1]]))
+
+    # print("real ground state energy=", eng[0])
+
+    hamiltonian = [ham0.qutip_op + hamExtX.qutip_op]
+
+    for i in range(l):
+        drive_z = Driving(
+            h=h,
+            dt=time[1] - time[0],
+            idx=i,
+        )
+
+        hamiltonian.append([obs[i], drive_z.field])
+
+    # evolution and
+
+    output = qutip.sesolve(hamiltonian, psi0, time, e_ops=obs + current_obs)
+
+    current_exp = np.zeros((steps, l))
+    z_exp = np.zeros_like(current_exp)
+
+    for r in range(l):
+        z_exp[:, r] = output.expect[r]
+        current_exp[:, r] = output.expect[l + r]
+
+    # Current derivative
+    current_derivative = np.gradient(current_exp, time, axis=0)
+
+    # compute the effective field
+    x_sp = np.sqrt(1 - z_exp**2) * np.cos(
+        np.arcsin(-1 * (current_exp) / (2 * np.sqrt(1 - z_exp**2)))
+    )
+
+    current_derivative = np.gradient(current_exp, time, axis=0)
+    h_eff = (0.25 * current_derivative + z_exp) / (x_sp + 10**-4)
+
+    # update the database
+    h_eff_tot.append(h_eff)
+    h_tot.append(h)
+    z_qutip_tot.append(z_exp)
+    current_qutip_tot.append(current_exp)
+    x_sp_tot.append(x_sp)
+    current_derivative_tot.append(current_derivative)
+
+    if idx % 1000 == 0:
+        np.savez(
+            f"data/dataset_h_eff/periodic/xxzx_model/dataset_random_rate_random_amplitude_01-08_fixed_initial_state_nbatch_{nbatch}_batchsize_{batch_size}_steps_{steps}_tf_{tf}_l_{l}_240620",
+            current=np.asarray(current_qutip_tot),
+            z=np.asarray(z_qutip_tot),
+            h_eff=np.asarray(h_eff_tot),
+            current_derivative=np.asarray(current_derivative_tot),
+            h=np.asarray(h_tot),
+            x_sp=np.asarray(x_sp_tot),
+            time=time,
+        )
 
 np.savez(
     f"data/dataset_h_eff/periodic/xxzx_model/dataset_random_rate_random_amplitude_01-08_fixed_initial_state_nbatch_{nbatch}_batchsize_{batch_size}_steps_{steps}_tf_{tf}_l_{l}_240620",
