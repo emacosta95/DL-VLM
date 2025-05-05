dd6a927cccf76c63dabbc4c5cb0526e4b80392d7
emacosta95 <emanuelecosta95@hotmail.com>
2025-05-05 17:10:09 +0200

    implement a TDVP code

--- TensorNetworkTimeEvolutionTDVP.jl
+++ TensorNetworkTimeEvolutionTDVP.jl
@@ -1,18 +1,19 @@
 #Imports
 using ITensors
 using Random, Distributions, LinearAlgebra
+using ITensorTDVP
 
 
 # Function to measure global magnetization
 function magnetization(psi, sites)
     x_sum = 0.0
     for i in 1:L
-        x_sum += expect(psi, "X"; site=i)
+        x_sum += mean(expect(psi, "X"; site=i))
     end
     return x_sum / length(sites)
 end
 
-global h 
+
 
 # Parameters
 L = 20             # System size
@@ -26,23 +27,23 @@ rate_min=0.
 amplitude_max=2.
 amplitude_min=0.
 rate_cutoff=10
-
+num_steps = Int(tmax/dt)
 # Build up the longitudinal field
 # Generate random values for rate and delta
 rate = rand(Uniform(rate_min, rate_max), rate_cutoff)
 delta = rand(Uniform(amplitude_min, amplitude_max), rate_cutoff)
 
+time = range(0, step=dt, length=num_steps)
 # Compute h using broadcasting
 h= zeros(Int(tmax/dt))
 #print(h)
 for i in 1:rate_cutoff
-    h =h+ delta[i] .* (cos.(π .+ time .* rate[i]) .+ 1) 
-    print(i)
+    global h =h+ delta[i] .* (cos.(π .+ time .* rate[i]) .+ 1) 
 end
 h=h/rate_cutoff
 print("h shape=$(size(h))")
 # Initialize the site indices for spin-1/2 systems
-sites = siteinds("S=1/2", L)
+sites = siteinds("S=1/2", L;conserve_qns=false)
 
 # initial state
 state=["Up" for n in 1:L]
@@ -58,31 +59,41 @@ cutoff!(sweeps, 1E-10)
 t = 0.0
 x_in_time=Float64[]
 
+ampo_0 = OpSum()
+for i in 1:L-1
+    global ampo_0 += -J, "Z", i, "Z", i+1
+end
+for i in 1:L
+    global ampo_0 += -omega, "Z", i
+    
+end
+
+ampo_1 =OpSum()
+for i in 1:L
+    global ampo_1 += -1, "X", i
+end
 
 while t < tmax
-    h_current = h_t(t)  # Compute h(t)
+    h_current = h[Int(round(t/dt)) + 1]  # Compute h(t)
 
+    print("h_current=$h_current")
     # Update the Hamiltonian with new h(t)
-    ampo = OpSum()
-    for i in 1:L-1
-        ampo += -J, "Z", i, "Z", i+1
-    end
-    for i in 1:L
-        ampo += -omega, "Z", i
-        ampo += -h_current, "X", i
-    end
-    H = MPO(ampo, sites)
+    ampo=ampo_0 + h_current*ampo_1
+    hamiltonian=MPO(ampo,sites)
+    
+
 
     # Evolve the state using TDVP
-    psi = tdvp(psi, H, -im * dt; sweeps=sweeps)
-
+    global psi = tdvp( hamiltonian, -im * dt, psi; maxdim=30,cutoff=1e-8,normalize=true, reverse_step=false,outputlevel=1)
+ 
     # Compute expectation values
+    x_center = mean(expect(psi, "X"; site=L÷2))
+    print(x_center)
     x_ave = magnetization(psi, sites)
-    x_center = expect(psi, "X"; site=L÷2)
     push!(x_in_time,x_ave)
     println("t = $t, x_ave = $x_ave, Z_center = $x_center")
 
-    t += dt
+    global t += dt
 end
 
 
